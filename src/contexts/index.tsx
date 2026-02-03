c96bffb1cfd11e615b6eb6f7c08e99000c65121b
Weslei Possidonio <weslei.possidonio@gmail.com>
2026-02-03 15:58:32 -0300

    feat:created UserContext, utils, UserTypes

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,10 +1,17 @@
 import type { ReactNode } from "react"
-
-
+import { ProductContextProvider } from "./ProductContext"
+import { UserContextProvider } from "./UserContext"
 interface AppProviderProps {
   children: ReactNode
 }
+
 export const AppProvider = ({ children }: AppProviderProps) => {
-  return children
+  return (
+    <UserContextProvider>
+      <ProductContextProvider>
+        {children}
+      </ProductContextProvider>
+    </UserContextProvider>
+  )
 }
 
