8f3c2e9b4fe1c6816f4f837362a8f91b680e1d2c
Weslei Possidonio <weslei.possidonio@gmail.com>
2026-02-03 15:40:41 -0300

    feat: created AppProvider

---
+++ src/contexts/index.tsx
@@ -0,0 +1,10 @@
+import type { ReactNode } from "react"
+
+
+interface AppProviderProps {
+  children: ReactNode
+}
+export const AppProvider = ({ children }: AppProviderProps) => {
+  return children
+}
+
